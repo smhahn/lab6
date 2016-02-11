208fadd6b9b51237e74184cd9c7455123ca32b89
Samantha Hahn <smhahn@ucsd.edu>
2016-02-11 09:25:59 -0800

    AJAX lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -22,11 +22,20 @@ function addProjectDetails(e) {
 	e.preventDefault();
 
 	// Get the div ID, e.g., "project3"
-	var projectID = $(this).closest('.project').attr('id');
+	var project = $(this).closest('.project');
+	var projectID = project.attr('id');
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+	console.log("/project/" + idNumber);
+	$.get("/project/" + idNumber, function(result) {
+		console.log(result);
+		var projectDetails = "<img class='detailsImage' src='" + result.image + "'/>";
+		projectDetails += "<h4>" + result.date + "</h4>";
+		projectDetails += "<p>" + result.summary + "</p";
+		project.find('.details').html(projectDetails);
+	});
 }
 
 /*
@@ -35,4 +44,13 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	$.get("/palette", function(result) {
+		console.log(result);
+		var colors = result.palette.colors.hex;
+		$('body').css('background-color', colors[0]);
+		$('.thumbnail').css('background-color', colors[1]);
+		$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+		$('p').css('color', colors[3]);
+		$('.project img').css('opacity', .75);
+	});
 }
